1dbda94a2e0cbc996cf5d397807207c29923a4a1
Joey Zhu <joey.j.zhu@berkeley.edu>
2022-11-23 18:21:45 -0800

    Skeleton code for Network

--- model/network.py
+++ model/network.py
@@ -1,1 +1,22 @@
 # TODO: Combine smaller modules into the full network here
+
+class Network:
+    def __init__(self, props):
+        self.x = None
+
+    def trainNetwork(self, data, props):
+        for i in range(len(data)):
+            self.forward()
+            self.backward()
+        return None
+
+    def testNetwork(self, data, props):
+        for i in range(len(data)):
+            self.forward()
+        return None
+
+    def forward(self, x):
+        return None
+
+    def backward(self, grad):
+        return None
